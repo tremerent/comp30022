f9f43202c19c4ca70671f4a0951a6276eca3a744
jsolem <jonahsol.e.m@gmail.com>
2019-08-22 21:28:23 +1000

    Init. Grandma's vase

---
+++ artefactor/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Artefactor.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artefactor.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+
+        }
+
+        public DbSet<Artefact> Artefacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<FamilyUserJoin>()
+            //            .HasKey(fuj => new { fuj.UserId, fuj.FamilyId });
+
+
+
+            modelBuilder.Entity<Artefact>()
+                        .Property(a => a.Genre)
+                        .HasConversion<string>();
+        }
+    }
+}
