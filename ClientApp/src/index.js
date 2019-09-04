6ac7ba35099df87793c97d135c8f240e06f80e82
tremerent <null@example.com>
2019-09-04 22:15:17 +1000

    Include bootstrap scripts to fix Carousel.

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-select/dist/js/bootstrap-select.js';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
