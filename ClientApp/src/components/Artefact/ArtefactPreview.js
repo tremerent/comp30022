9616d17527b78890c1048713d8a1d099f232ee96
sam <null@example.com>
2019-09-05 23:10:38 +1000

    Scroller is a long list generated from props.
    ArtefactPreviews now take their images from the 'artefact' object -- the idea
    being that until we implement image upload they will just be populated with
    placeholder images. This is currently implemented in 'ArtefactIndex.js', which
    just creates an ArtefactScroller component out of a bunch of example values.

--- ClientApp/src/components/Artefact/ArtefactPreview.js
+++ ClientApp/src/components/Artefact/ArtefactPreview.js
@@ -3,52 +3,38 @@ import React, { Component } from 'react';
 export class ArtefactPreview extends Component {
 
     render() {
-        let carouselId = `artefact-preview-carousel-${this.props.artefact.id}`;
+        let a = this.props.artefact;
+        let carouselId = `artefact-preview-carousel-${a.id}`;
+
        return (
 <div class="artefact-preview" style={{width: '18rem'}}>
     <div id={carouselId} class="carousel slide artefact-preview-carousel" data-ride="carousel" data-interval="false">
-      <ol class="carousel-indicators">
-        <li data-target={`#${carouselId}`} data-slide-to="0" class="active"></li>
-        <li data-target={`#${carouselId}`} data-slide-to="1"></li>
-        <li data-target={`#${carouselId}`} data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-              <img
-                    src='https://www.ricefurniture.com.au/content/images/thumbs/0002652_vintage-vase_550.jpeg'
-                    class="d-block"
-                    style={{ height: '12rem', margin: '0 auto' }}
-                    alt="THIS IS SOME ALT TEXT"/>
+        <ol class="carousel-indicators">
+            {Array.from(a.images.keys()).map(n => (
+                <li data-target={`#${carouselId}`} key={n} data-slide-to={n} class={n ? undefined : 'active'}></li>
+            ))}
+        </ol>
+        <div class="carousel-inner">
+            {Array.from(a.images.keys()).map(n => (
+                <div class={n ? 'carousel-item' : 'carousel-item active'}>
+                    <img src={a.images[n]} class='d-block' style={{ height: '12rem', margin: '0 auto' }} alt='TODO'/>
+                </div>
+            ))}
         </div>
-        <div class="carousel-item">
-              <img
-                    src="https://images.crateandbarrel.com/is/image/Crate/GeoVaseSHS17/?$web_product_hero$&190411135054&wid=625&hei=625"
-                    class="d-block"
-                    style={{ height: '12rem', margin: '0 auto' }}
-                    alt="THIS IS SOME ALT TEXT"/>
-        </div>
-        <div class="carousel-item">
-              <img
-                    src="https://images1.novica.net/pictures/4/p298885_2_400.jpg"
-                    class="d-block"
-                    style={{ height: '12rem', margin: '0 auto' }}
-                    alt="THIS IS SOME ALT TEXT"/>
-        </div>
-      </div>
-      <a class="carousel-control-prev" href={`#${carouselId}`} role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href={`#${carouselId}`} role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+        <a class="carousel-control-prev" href={`#${carouselId}`} role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href={`#${carouselId}`} role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
     <div className="card-body">
-        <h5 className="card-title">{this.props.artefact.name}</h5>
-        <p className="card-text">{this.props.artefact.description}</p>
+        <h5 className="card-title">{a.title}</h5>
+        <p className="card-text">{a.description}</p>
         <div className='card-text artefact-preview-view-more'>
-            <a href={`/artefact/${this.props.artefact.id}`}>
+            <a href={`/artefact/${a.id}`}>
                 View full page ▶ {/* <- U+25B6 */}
             </a>
         </div>
