ab371a4b9ec113d625ddbe9dd087059e8609782f
tremerent <null@example.com>
2019-09-19 07:25:06 +1000

    Some scroller stuff.

--- ClientApp/src/components/Artefact/ArtefactScroller.js
+++ ClientApp/src/components/Artefact/ArtefactScroller.js
@@ -1,22 +1,22 @@
 ﻿import React, { Component } from 'react';
 import { ArtefactPreview } from './ArtefactPreview.js';
 
-export default class ArtefactScroller extends Component {
+export class ArtefactScroller extends Component {
     constructor(props) {
         super(props);
         this.state = { };
     }
 
     render() {
-       return (
-           <div>
-               {this.props.artefacts.map(a => {
-                   return (
-                       <ArtefactPreview key={a.id} artefact={a} className="my-1" />
+        return (
+            <div style={{ height: '500px', overflowY: 'scroll' }}>
+                {this.props.artefacts.map(a => {
+                    return (
+                        <ArtefactPreview key={a.id} artefact={a} className="my-1" />
                    );
                })}
             </div>
-       );
+        );
    }
 }
 
