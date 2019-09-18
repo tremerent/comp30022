96af8581973e4696b0646a2758a0d8a47eab3f1d
tremerent <null@example.com>
2019-09-19 06:24:45 +1000

    Spit-and-prayer artefact browser implementation.

--- ClientApp/src/components/Artefact/ArtefactBrowser.js
+++ ClientApp/src/components/Artefact/ArtefactBrowser.js
@@ -1,15 +1,40 @@
 
 import React from 'react';
 import ArtefactScroller from './ArtefactScroller.js';
+import { getArtefacts } from '../../scripts/requests.js';
 
-export default function ArtefactBrowser(props) {
-    return (
-        <p>
-            yeah this'd be like a cool scroller thing and we'd make sure it
-            shows you all the correct artefacts that people have shared publicly
-        </p>
-    );
+import PLACEHOLDER_IMAGE_01 from '../../images/filler/artefact-01.jpg';
+import PLACEHOLDER_IMAGE_02 from '../../images/filler/artefact-02.jpg';
+import PLACEHOLDER_IMAGE_03 from '../../images/filler/artefact-03.jpg';
 
+export default class ArtefactBrowser extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            artefacts: [],
+            loading: true
+        };
+    }
+
+    componentDidMount() {
+        getArtefacts()
+            .then(artefacts => {
+                for (let a of artefacts)
+                    a.images = [
+                                PLACEHOLDER_IMAGE_01,
+                                PLACEHOLDER_IMAGE_02,
+                                PLACEHOLDER_IMAGE_03,
+                            ];
+                this.setState({ artefacts, loading: false });
+            });
+    }
+
+
+    render() {
+        return <ArtefactScroller artefacts={this.state.artefacts} class="col-xs-6" style={{ margin: 'auto' }}/>
+    }
 
 }
 
