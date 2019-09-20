a418dbb962eb88e1591d9b494d6357b5771c9327
tremerent <null@example.com>
2019-09-20 20:00:10 +1000

    Make navmenu floating (position: fixed).

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -53,37 +53,40 @@ export class NavMenu extends React.Component {
 
     render() {
         return (
-<header className='af-layout-top'>
-    <Navbar light className="navbar-expand-sm ng-white box-shadow">
-        <Container>
-            <NavbarBrand tag={Link} to="/">Artefactor</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNav}/>
-            <Collapse
-                className="d-sm-inline-flex flex-sm-row-reverse"
-                isOpen={!this.state.collapsed} navbar
-            >
-                <ul className="navbar-nav">
-                    <NavLink tag={Link} to="/browse">Browse</NavLink>
-                    {
-                        this.state.authenticated ? (
-                            <>
-                            {/*<NavLink tag={Link} to="/family">My Family</NavLink>*/}
-                            {/*<NavLink tag={Link} to="/profile">Profile</NavLink>*/}
-                            <NavLink tag={Link} to="/my-artefacts">My Artefacts</NavLink>
-                            {/*<NavLink tag={Link} to="/logout">Log Out</NavLink>*/}
-                            </>
-                        ) : (
-                            <>
-                            {/*<NavLink tag={Link} to="/login">Log In</NavLink>*/}
-                            {/*<NavLink tag={Link} to="/signup">Sign Up</NavLink>*/}
-                            </>
-                        )
-                    }
-                    <LoginMenu></LoginMenu>
-                </ul>
-            </Collapse>
-        </Container>
-    </Navbar>
+<header>
+    <div className='af-navmenu'>
+        <Navbar light className="navbar-expand-sm ng-white box-shadow">
+            <Container>
+                <NavbarBrand tag={Link} to="/">Artefactor</NavbarBrand>
+                <NavbarToggler onClick={this.toggleNav}/>
+                <Collapse
+                    className="d-sm-inline-flex flex-sm-row-reverse"
+                    isOpen={!this.state.collapsed} navbar
+                >
+                    <ul className="navbar-nav">
+                        <NavLink tag={Link} to="/browse">Browse</NavLink>
+                        {
+                            this.state.authenticated ? (
+                                <>
+                                {/*<NavLink tag={Link} to="/family">My Family</NavLink>*/}
+                                {/*<NavLink tag={Link} to="/profile">Profile</NavLink>*/}
+                                <NavLink tag={Link} to="/my-artefacts">My Artefacts</NavLink>
+                                {/*<NavLink tag={Link} to="/logout">Log Out</NavLink>*/}
+                                </>
+                            ) : (
+                                <>
+                                {/*<NavLink tag={Link} to="/login">Log In</NavLink>*/}
+                                {/*<NavLink tag={Link} to="/signup">Sign Up</NavLink>*/}
+                                </>
+                            )
+                        }
+                        <LoginMenu></LoginMenu>
+                    </ul>
+                </Collapse>
+            </Container>
+        </Navbar>
+    </div>
+    <div className='af-navmenu-placeholder'></div>
 </header>
         );
     }
