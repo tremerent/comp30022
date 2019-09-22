3f8948d531063ea28d1d5edc21a3eedfad9d7712
Jonah Elso-Mathews <jonahsol.e.m@gmail.com>
2019-09-22 16:27:13 +1000

    Patch not found style

--- ClientApp/src/components/NotFound.js
+++ ClientApp/src/components/NotFound.js
@@ -6,15 +6,15 @@ import './NotFound.css';
 export default class NotFound extends Component {
     render() {
         return (
-            <div className="af-not-found-box container">
-                <div className="af-not-found-content card text-white bg-danger mb-3">
+            <div className="af-not-found-outer">
+                <div className="af-not-found-inner card text-white bg-danger mb-3">
                     <div className="card-header">404</div>
                     <div className="card-body">
-                        <h4 className="card-title">Sorry! Page not found</h4>
+                        <h4 className="card-title">Sorry!<br />Page not found.</h4>
                         <Link to="/">
                             <button className="btn btn-primary my-3">
                                 Home
-                        </button>
+                            </button>
                         </Link>
                     </div>
                 </div>
