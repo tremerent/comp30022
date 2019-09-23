fe90855b9a96c128e11b48b9278345296136982a
tremerent <null@example.com>
2019-09-24 03:35:31 +1000

    MyArtefacts shows newly created artefacts.
    Total hack; should be fixed properly at a more opportune time.

--- ClientApp/src/components/Artefact/ArtefactScroller.js
+++ ClientApp/src/components/Artefact/ArtefactScroller.js
@@ -12,23 +12,24 @@ import PLACEHOLDER_IMAGE_03 from '../../images/filler/artefact-03.jpg';
 export default class ArtefactScroller extends Component {
     constructor(props) {
         super(props);
+
+        uet artefacts = this.props.artefacts;
         this.state = {
-            artefacts: [],
-            loading: true
+            artefacts: Array.isArray(artefacts) && artefacts.length ? (
+                    artefacts
+                ) : (
+                    []
+                ),
         };
+        this.state.loading = !this.state.artefacts.length;
     }
 
     componentDidMount() {
-        getArtefacts()
-            .then(artefacts => {
-                for (let a of artefacts)
-                    a.images = [
-                                PLACEHOLDER_IMAGE_01,
-                                PLACEHOLDER_IMAGE_02,
-                                PLACEHOLDER_IMAGE_03,
-                            ];
-                this.setState({ artefacts, loading: false });
-            });
+        if (this.state.loading)
+            getArtefacts()
+                .then(artefacts => {
+                    this.setState({ artefacts, loading: false });
+                });
     }
 
     render() {
@@ -42,8 +43,14 @@ export default class ArtefactScroller extends Component {
             //        <div className='af-artefact-scroller-inner'>
                     <div className='af-artefact-srcoller-container'>
                         {this.state.artefacts.map(a => {
-                            return a &&
-                                <ArtefactPreview key={a.id} artefact={a} />
+                            if (a) {
+                                a.images = [
+                                    PLACEHOLDER_IMAGE_01,
+                                    PLACEHOLDER_IMAGE_02,
+                                    PLACEHOLDER_IMAGE_03,
+                                ];
+                                return <ArtefactPreview key={a.id} artefact={a} />;
+                            }
                         })}
                     </div>
             //        </div>
