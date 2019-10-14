5eceb99f7b4efcd541c2298c26581da1edfd2e2d
highhaploid <54368721+highhaploid@users.noreply.github.com>
2019-10-14 23:15:26 +1100

    Update Discussion.js

--- ClientApp/src/components/Discussion.js
+++ ClientApp/src/components/Discussion.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { ShowComment } from './ShowComment.js'
+import { ShowComment } from './ShowComment.js';
+import './Question.css';
+import './Comment.css';
 
 export class Discussion extends Component {
     constructor(props) {
@@ -15,29 +17,39 @@ export class Discussion extends Component {
      /*When the comment box is typed in and entered,
      add a comment.*/
      add_comment(e) {
+
          comment_count = comment_count + 1;
      }
 
      /*Displays the given comment as well as all its descendents */
      display_comments(c) {
+         var queryType;
+         if (c.type == "normal") {
+             queryType = "Comment";
+         }
+         else if (c.type == "question") {
+             queryType = "Question";
+         }
+
          const childrenList = c.children.map(child => {
-          return(
-              <li key={child.id}>{this.display_comments(child)}</li>
-          )
-     })
-     return(
-        <div>
-          <ShowComment comment = {c}/>
-          <ul>{childrenList}</ul>
-        </div>
-      );
+         return(
+             <li key={child.id}>{this.display_comments(child)}</li>
+         )
+         })
+
+         return(
+             <div  className = {queryType}>
+                 <ShowComment comment = {c}/>
+                 <ul>{childrenList}</ul>
+             </div>
+         );
     }
 
-  render() {
-      return(
+    render() {
+        return(
         <div>
              {this.display_comments(this.state.comments)}
         </div>
-      );
+        );
     }
 }
