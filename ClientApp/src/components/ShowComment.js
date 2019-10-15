6b6c2fbf264d04cfbe6df86a276feccb4fb647e4
highhaploid <54368721+highhaploid@users.noreply.github.com>
2019-10-16 01:15:38 +1100

    Update ShowComment.js

--- ClientApp/src/components/ShowComment.js
+++ ClientApp/src/components/ShowComment.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function ShowComment(props) {
+    var type;
+    type = props.comment.type;
     return (
-        <div>
+        <div className={type}>
             <p><a>{props.comment.user}</a>: {props.comment.content}</p>
         </div>
     );
