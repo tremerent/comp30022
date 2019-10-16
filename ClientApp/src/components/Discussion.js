f602cac44892ff02167c0f1a31bf05edeb4ef657
highhaploid <54368721+highhaploid@users.noreply.github.com>
2019-10-17 02:57:16 +1100

    fix some bugs

--- ClientApp/src/components/Discussion.js
+++ ClientApp/src/components/Discussion.js
@@ -8,48 +8,45 @@ export class Discussion extends Component {
 
         this.state = {
             comments : props.comments,
-            comment_count : count_comments(props.comments)
+            comment_count : this.count_comments(props.comments)
         }
 
-     }
-
-     count_comments(comment_list) {
-         count = 0;
-         for (comment in comment_list) {
-             count = count + count_comments(comment.children) + 1;
-         }
-         return count;
-     }
-
-     /*When the comment box is typed in and entered,
-     add a comment.*/
-     add_comment(e) {
-
-         comment_count = comment_count + 1;
-     }
-
-     /*Displays the given comment as well as all its descendents */
-     display_comments(c) {
-
-         const childrenList = c.children.map(child => {
-         return(
-             <li key={child.id}>{this.display_comments(child)}</li>
-         )
-         })
-
-         return(
-             <div>
-                 <ShowComment comment = {c}/>
-                 <ul>{childrenList}</ul>
-             </div>
-         );
     }
+
+    /*Counts the number of comments in the comment list, including descendents*/
+    count_comments(comment_list) {
+        var count = 0;
+        for (var comment in comment_list) {
+              count = count + this.count_comments(comment.children) + 1;
+        }
+        return count;
+    }
+
+    /*When the comment box is typed in and entered,
+    add a comment.*/
+    add_comment(e) {
+
+        comment_count = comment_count + 1;
+    }
+
+    /*Displays the list of comments as well as all its descendents */
+    display_comments(c) {
+        const childrenList = c.map(child => {
+            return(
+                <ul key={child.id}>
+                     <li><ShowComment comment={child}/></li>
+                     <li>{this.display_comments(child.children)}</li>
+                </ul>
+            )
+        })
+        return(<div>{childrenList}</div>);
+        }
 
     render() {
         return(
-        <div>
-             {this.display_comments(this.state.comments)}
-        </div>
+            <div>
+                {this.display_comments(this.state.comments)}
+            </div>
         );
     }
 }
