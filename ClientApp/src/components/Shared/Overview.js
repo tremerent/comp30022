52150e459bfbd164bb1c4a81a0834518413508f4
tremerent <null@example.com>
2019-10-19 14:57:32 +1100

    Finish fixing profile.
    Still a small bug where the page is slightly too long for viewport but
    fuck it.

--- ClientApp/src/components/Shared/Overview.js
+++ ClientApp/src/components/Shared/Overview.js
@@ -14,14 +14,33 @@ export default class Overview extends React.Component {
                 'Overview: this component should have exactly 2 children, but '
                 +   `has been given ${this.props.children.length}.`
             );
+
+        const { sizeStatic, sizeScroll } = props;
+
+        if (sizeStatic !== undefined) {
+            this.sizeStatic = sizeStatic;
+            if (sizeScroll === undefined)
+                this.sizeScroll = `calc(100% - ${sizeStatic})`;
+            else
+                this.sizeScroll = sizeScroll;
+        } else if (sizeScroll !== undefined) {
+            this.sizeStatic = `calc(100% - ${sizeScroll})`;
+            this.sizeScroll = sizeScroll;
+        } else {
+            this.sizeStatic = '50%';
+            this.sizeScroll = '50%';
+        }
+
+        console.log(`sizeStatic: ${this.sizeStatic}, sizeScroll: ${this.sizeScroll}`);
     }
 
     render() {
+
         return (
             <>
             <div className='af-ov-frame'>
                 <div className='container'>
-                    <div className='af-ov-static'>
+                    <div className='af-ov-static' style={{ width: this.sizeStatic }}>
                         <ScrollBox>
                             <div className='af-ov-navbar-placeholder'/>
                             {this.props.children[0]}
@@ -29,7 +48,7 @@ export default class Overview extends React.Component {
                     </div>
                 </div>
             </div>
-            <div className='af-ov-scroll'>
+            <div className='af-ov-scroll' style={{ left: `calc(100% - ${this.sizeScroll})`, width: this.sizeScroll }}>
                 {this.props.children[1]}
             </div>
             </>
--- ClientApp/src/components/User/UserProfile.js
+++ ClientApp/src/components/User/UserProfile.js
@@ -14,34 +14,7 @@ import '../Nav/NavMenu.css';
 
 import Overview from '../Shared/Overview.js';
 
-const FILL = `
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit
-esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-voluptas nulla pariatur?
-At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
-excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem
-rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis
-est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
-possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
-autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
-ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum
-rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
-alias consequatur aut perferendis doloribus asperiores repellat.
-THE END
-`;
-
-export default class UserProfile extends React.Component {
+class UserInfo extends React.Component {
 
     changeBio = (newBio) => {
         this.props.updateUserDetails({
@@ -50,7 +23,6 @@ export default class UserProfile extends React.Component {
     }
 
     render() {
-
         const BioText = (props) => {
             const bioPlaceholderStr =
                 `Oh no! ${this.props.user.username} is yet to provide a bio.`;
@@ -69,38 +41,72 @@ export default class UserProfile extends React.Component {
         }
 
         return (
-            <Overview>
-                <div className='af-profile-card-inner'>
-                    <ProfilePicture
-                        imageUrl={this.props.user.imageUrl}
-                        updateProfilePic={this.props.
-                            updateUserProfilePic
-                        }
-                        editable={this.props.editable}
-                    />
-                    <div className='af-profile-info'>
-                        <h2 className='af-profile-name'>{this.props.user.username}</h2>
-                        <div className='af-profile-badges'>
-                            <span className="badge badge-decal-text mx-1 af-profile-art-badge">
-                                {(this.props.numArtefactsReg
-                                    ? this.props.numArtefactsReg
-                                    : 0) + ' Artefacts'}
-                            </span>
-                        </div>
-                        <hr/>
-                        {
-                            this.props.editable
-                            ?
-                            <EditableBio
-                                value={this.props.user.bio}
-                                onValueSubmit={this.changeBio}
-                            />
-                            :
-                            <BioText />
-                        }
-                    </div>
+            <div className='af-profile-info'>
+                <ProfilePicture
+                    imageUrl={this.props.user.imageUrl}
+                    updateProfilePic={this.props.
+                        updateUserProfilePic
+                    }
+                    editable={this.props.editable}
+                />
+                <h2 className='af-profile-name'>{this.props.user.username}</h2>
+                <div className='af-profile-badges'>
+                    <span className="badge badge-decal-text mx-1 af-profile-art-badge">
+                        {(this.props.numArtefactsReg
+                            ? this.props.numArtefactsReg
+                            : 0) + ' Artefacts'}
+                    </span>
                 </div>
-                <div>{FILL}</div>
+                <hr/>
+                {
+                    this.props.editable
+                    ?
+                    <EditableBio
+                        value={this.props.user.bio}
+                        onValueSubmit={this.changeBio}
+                    />
+                    :
+                    <BioText />
+                }
+            </div>
+        );
+    }
+}
+
+class UserScroller extends React.Component {
+
+    render() {
+        return (
+            <>
+            <FloatingWindow id="addart" className='af-register-modal' title='Register An Artefact'>
+                <CreateArtefacts/>
+            </FloatingWindow>
+            <div className='af-profile-scroller'>
+                <hr/>
+                <div className='af-profile-scroller-title'>
+                    <h3 style={{ display: 'inline' }}>{this.props.user.username + "'s Artefacts"}</h3>
+                    <button className='btn btn-primary af-profile-addbutton' data-target='#addart' data-toggle='modal'>
+                        Add
+                    </button>
+                </div>
+                <hr/>
+                <ArtefactScroller
+                    artefacts={this.props.userArtefacts}
+                    placeholder={"Oh no! This user hasn't registered any artefacts yet."}
+                />
+            </div>
+            </>
+        );
+    }
+
+}
+
+export default class UserProfile extends React.Component {
+    render() {
+        return (
+            <Overview sizeStatic='50%' sizeScroll='46%'>
+                <UserInfo {...this.props}/>
+                <UserScroller {...this.props}/>
             </Overview>
         );
     }
