208c213a2dbc71e49ace8fc91bd751c8edfab45d
jsolem <jonahsol.e.m@gmail.com>
2019-10-30 23:06:23 +1100

    Signup redir

--- ClientApp/src/components/Auth/AuthLayout.js
+++ ClientApp/src/components/Auth/AuthLayout.js
@@ -29,31 +29,5 @@ export default function AuthLayout(props) {
                 </div>
             </div>
         );
-
-        // terrible banner thing
-        //    /*.af-auth-row {
-        //margin-top: 5vh;
-
-        //display: flex;
-        //flex-direction: row;
-        //}*/
-        //return (
-        //    <div className="af-auth-outer">
-        //        <div className="af-auth-row">
-        //            <div className="af-auth-form">
-        //                <Component />
-        //            </div>
-        //            <div className="af-auth-decoration">
-        //                <div className="af-auth-decoration-outer">
-        //                    <div className="af-auth-decoration-box box-shadow">
-        //                        <div className="af-auth-outer-brand">
-        //                            <img src={ARTEFACTOR_BRAND} className="af-auth-brand" />
-        //                        </div>
-        //                    </div>
-        //                </div>
-        //            </div>
-        //        </div>
-        //    </div>
-        //);
     }
 }
